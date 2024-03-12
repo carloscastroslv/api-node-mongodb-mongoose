736a46e91beb62e933d13046816401961cea3012
carloscastroslv <160295679+carloscastroslv@users.noreply.github.com>
2024-03-12 17:11:53 -0300

    atualizando conn

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
 async function main(){
     try {
         mongoose.set("strictQuery", true)
-        // const DB_NAME = process.env.DB_NAME
-        // const DB_USERNAME = process.env.DB_USERNAME
-        // const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+        const DB_NAME = process.env.DB_NAME
+        const DB_USERNAME = process.env.DB_USERNAME
+        const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
         await mongoose.connect(
             // colocar as credenciais do banco de dados no link abaixo.
-            "mongodb+srv://carlosantoninoacs:7lXDziJ9kTjFSiMr@cluster0.d2xtptm.mongodb.net/bancoapi?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@cluster0.d2xtptm.mongodb.net/{DB_NAME}?retryWrites=true&w=majority&appName=Cluster0"
             );
 
         console.log("Conectado ao banco");
